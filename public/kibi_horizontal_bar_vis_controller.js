178e6f239129ef14f9ef5607df0e0851b03416f9
nguyendanghung <nguyendanghung2008@gmail.com>
2016-03-28 16:15:14 +0700

    refractor code

--- public/kibi_horizontal_bar_vis_controller.js
+++ public/kibi_horizontal_bar_vis_controller.js
@@ -2,21 +2,18 @@ define(function (require) {
   // get the kibana/metric_vis module, and make sure that it requires the "kibana" module if it
   // didn't already
   // var module = require('ui/modules').get('kibana/kibi_horizontal_bar_vis', ['kibana','nvd3ChartDirectives']);
-  var module = require('ui/modules').get('kibana/kibi_horizontal_bar_vis', ['kibana','nvd3']);
+  var module = require('ui/modules').get('kibana/kibi_horizontal_bar_vis', ['kibana', 'nvd3']);
   var d3 = require('d3');
+  var _ = require('lodash');
 
   module.controller('KbnHorizontalBarVisController', function ($scope, $element, $rootScope, Private) {
-    var tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
 
-    var data, config;
+    var tabifyAggResponse = Private(require('ui/agg_response/tabify/tabify'));
+    var config;
     var margin, width, height;
 
     // declare data
     var tableGroups = null;
-
-    var svg_root = $element[0] ; 
-    // d3.select($element[0]).selectAll("svg").remove();
-
     var _updateDimensions = function () {
       var delta = 18;
       var w = $element.parent().width();
@@ -36,60 +33,15 @@ define(function (require) {
     };
 
     var off = $rootScope.$on('change:vis', function () {
-        // console.log("change vis");
+
       _updateDimensions();
-      // _initConfig();
       $scope.processTableGroups(tableGroups);
       _updateConfig();
+
     });
 
     $scope.$on('$destroy', off);
 
-  //   $scope.options = {
-  //     chart: {
-  //         type: 'multiBarHorizontalChart',
-  //         width: config.w - config.margin,
-  //         height: config.h - config.margin,
-  //         margin : {
-  //           top : 20,
-  //           left : 100,
-  //           bottom : 50
-  //         },
-  //         x: function(d){return d.label;},
-  //         y: function(d){return d.value;},
-  //         showControls: false,
-  //         stacked: false,
-  //         tooltips: true,
-  //         showValues: true,
-  //         duration: 500,
-  //         xAxis: {
-  //             showMaxMin: false
-  //         },
-  //         yAxis: {
-  //             axisLabel: 'Values',
-  //             tickFormat: function(d){
-  //                 return d3.format(',.2f')(d);
-  //             }
-  //         },                
-  //         dispatch: {
-  //             elementClick: function(e){ console.log('click') },
-  //             elementMouseover: function(e){ console.log('mouseover') },
-  //             elementMouseout: function(e){ console.log('mouseout') },
-  //             renderEnd: function(e){ console.log('renderEnd') }
-  //         },
-  //         legend: {
-  //             dispatch: {
-  //                 legendClick: function(e) {
-  //                   console.log("legend click");
-  //                   _updateConfig();
-  //                 }
-  //             }
-  //         }                
-  //     }
-  // };
-    
-
-    var chart;   
 
     $scope.processTableGroups = function (tableGroups) {
       $scope.data = [];
@@ -106,27 +58,25 @@ define(function (require) {
           }
           group['key'] = groupKey;
           var values = [];
-          for (var j = 0; j < rows.length; j++) {
-            var row = rows[j];
+          _.each(rows, function (row) {
             var item = {};
             item.label = row[0];
             item.value = row[i];
             values.push(item);
-          }
+          });
           group.values = values;
           $scope.data.push(group);
         }
-
       });
     };
 
-  // set default config  
-    var _initConfig = function(){
-        
+    // set default config
+    var _initConfig = function () {
+
       config = {
-        margin : 50,
+        margin: 50,
         w: width - margin,
-        h: height - margin,        
+        h: height - margin,
         showAxesLabels: true,
         showAxes: true,
         showLegend: true,
@@ -135,67 +85,79 @@ define(function (require) {
         showTooltips: true,
         showStacked: false,
         showGrouped: true
-    };
-  }
+      };
+    }
 
-   var _updateConfig = function() {    
-        config.showLegend = $scope.vis.params.addLegend;
-        config.showTooltips = $scope.vis.params.addTooltip;
-        config.showControls = $scope.vis.params.addControl;
-        config.w = width;
-        config.h = height;
-        config.showValues = $scope.vis.params.addValues;
+    var _updateConfig = function () {
+      config.showLegend = $scope.vis.params.addLegend;
+      config.showTooltips = $scope.vis.params.addTooltip;
+      config.showControls = $scope.vis.params.addControl;
+      config.w = width;
+      config.h = height;
+      config.showValues = $scope.vis.params.addValues;
 
-        $scope.options = {
-            chart: {
-                type: 'multiBarHorizontalChart',
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
-                width: config.w - config.margin,
-                height: config.h - config.margin,
-                margin : {
-                    top : 20,
-                    left : 100,
-                    bottom : 50
-                },
-                showControls: config.showControls,
-                showValues: config.showValues,
-                // stacked: false,
-                // grouped: true,
-                showLegend: config.showLegend,
-                tooltips: config.showTooltips,
-                duration: 500,
-                xAxis: {
-                    showMaxMin: false
-                },
-                yAxis: {
-                    axisLabel: 'Values',
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    }
-                },                
-                dispatch: {
-                    elementClick: function(e){ console.log('click') },
-                    elementMouseover: function(e){ console.log('mouseover') },
-                    elementMouseout: function(e){ console.log('mouseout') },
-                    renderEnd: function(e){ console.log('renderEnd') }
-                },
-                legend: {
-                    dispatch: {
-                        legendClick: function(e) {
-                          // console.log("legend click");
-                          // _updateConfig();
-                        }
-                    }
-                }                
+      $scope.options = {
+        chart: {
+          type: 'multiBarHorizontalChart',
+          x: function (d) {
+            return d.label;
+          },
+          y: function (d) {
+            return d.value;
+          },
+          width: config.w - config.margin,
+          height: config.h - config.margin,
+          margin: {
+            top: 20,
+            left: 100,
+            bottom: 50
+          },
+          showControls: config.showControls,
+          showValues: config.showValues,
+          // stacked: false,
+          // grouped: true,
+          showLegend: config.showLegend,
+          tooltips: config.showTooltips,
+          duration: 500,
+          xAxis: {
+            showMaxMin: false
+          },
+          yAxis: {
+            axisLabel: 'Values',
+            tickFormat: function (d) {
+              return d3.format(',.2f')(d);
             }
-        };      
+          },
+          dispatch: {
+            elementClick: function (e) {
+              console.log('click')
+            },
+            elementMouseover: function (e) {
+              console.log('mouseover')
+            },
+            elementMouseout: function (e) {
+              console.log('mouseout')
+            },
+            renderEnd: function (e) {
+              console.log('renderEnd')
+            }
+          },
+          legend: {
+            dispatch: {
+              legendClick: function (e) {
+                // console.log("legend click");
+                // _updateConfig();
+              }
+            }
+          }
+        }
+      };
     };
 
     $scope.$watch('esResponse', function (resp) {
       _initConfig();
       if (resp) {
-        tableGroups = tabifyAggResponse($scope.vis, resp);    
+        tableGroups = tabifyAggResponse($scope.vis, resp);
         _updateDimensions();
         $scope.processTableGroups(tableGroups);
         _updateConfig();
